afcb6e1a7c8676d36eee89d1489a7c54c6e139da
cdufour <opusidea@gmail.com>
2023-08-31 18:47:08 +0200

    j1 end

--- hello.c
+++ hello.c
@@ -1,7 +1,19 @@
+/*
+Formation:    Programmation C
+Prof:         Christophe Dufour
+*/
+
+
 #include <stdio.h>
 
+// fonction principale (obligatoire)
 int main() {
-  printf("Hello World !");
-  printf("\n");
-  return 7; // return code ; 0: ok, success
+  
+  printf("Hello World !\n");
+  printf("Voici un backslash: \\\n");
+
+  //printf("\"Guillemets\"\n");
+  
+  //return code ; 0: ok, success
+  return 0; 
 }
---
+++ variables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    int n = 5;
+    printf("Valeur de n: %d\n", n);
+
+    n = 9;
+    printf("Valeur de n: %d\n", n);
+
+    float pi = 3.14;
+    printf("Valeur de pi: %f\n", pi);
+
+    return 0;
+}
